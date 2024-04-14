584c77c73285416ae157ca977343b6894b858721
abdrasaq14 <abdrasaq14@gmail.com>
2024-04-14 15:56:39 +0100

    header fixed

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,15 +1,15 @@
 import { LogoImage } from "../../assets/index";
 import { navBarItem } from "..";
-import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
-        <div className="w-full flex items-center justify-between bg-white">
-            <div className="w-[20%] flex items-center justify-center">
-                <img src={LogoImage} className="" alt="" />
+        <div className="w-full flex items-center justify-between bg-white h-[6rem] rounded-[10px]">
+            <div className="w-[20%] flex items-center justify-center bg-sky-600 p-3">
+                <img src={LogoImage} className="max-w-[100%] w-50" alt="" />
             </div>
             <div className="flex items-center justify-between">
-                {navBarItem.map((item, index) => (
-                    <Link to={`${item.link}`}>{item.name }</Link>
+                {navBarItem.map((item) => (
+                    <a key={item.name} href={`${item.link}`} className="text-white">{item.name }</a>
             ))}    
             </div>
             <div className="bg-primary">
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,15 +1,14 @@
 import { BusImage } from "../../assets/index";
+import Header from "./Header";
 const Hero = () => {
     return (
-        <div className="bg-overlay h-[100vh] w-full" style={{
+        <div className="bg-overlay h-[100vh] w-full md:px-15 lg:px-[5rem] py-6 flex flex-col" style={{
             '--overlay-image': `url(${BusImage})`,
             '--overlay-colors': "rgba(5, 5, 5, 0.5), rgba(5, 5, 5, 0.7)",
             '--overlay-angle': "90deg"
         } as React.CSSProperties}>
-            <div className="hero-content">
-                <h1>Bus Ticket Booking</h1>
-                <p>Book your tickets in advance to avoid the rush</p>
-                <button>Book Now</button>
+            <div className="w-full">
+                <Header/>
             </div>
         </div>
     );
